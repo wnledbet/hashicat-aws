e803c6184100e4e5a844e35b2f0f26b4153efc64
Nolan Ledbetter <Nolan.Ledbetter@Phoenix.edu>
2020-04-10 23:30:27 +0000

    Added VPC module

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+# test
+module "vpc" {
+  source  = "app.terraform.io/NolanLedbetter-training/vpc/aws"
+  version = "2.33.0"
+
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+}
