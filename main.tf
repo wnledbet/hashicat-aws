a87204f76ee1750d64bc78d1e48a79edf69d2fec
Nolan Ledbetter <Nolan.Ledbetter@Phoenix.edu>
2020-04-10 23:54:17 +0000

    Added VPC module

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ resource aws_instance "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Billing = "True"
+    Billable = "True"
     Department = "devops"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,5 @@
 module "vpc" {
   source  = "app.terraform.io/NolanLedbetter-training/vpc/aws"
   version = "2.33.0"
-
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
-
-  tags = {
-    Name = "main-nolan"
+  cidr = "10.0.0.0/16"
 }
